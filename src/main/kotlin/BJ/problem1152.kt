796be39e0b7383e7878bf39ecfd398969765aba5
JasonYoo1995 <jasonyoo950909@gmail.com>
2024-09-16 14:25:08 +0900

    remove reflection

--- src/main/kotlin/BJ/problem1152.kt
+++ src/main/kotlin/BJ/problem1152.kt
@@ -2,7 +2,7 @@ package BJ
 
 // https://www.acmicpc.net/problem/1152
 
-fun solution() {
+fun main() {
     val ans = readln()
         .trim() // 좌우 공백 제거
         .split(" ") // 단어 단위로 쪼개기 : Empty String도 하나의 요소로 취급되므로 주의
